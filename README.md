1dcd7c406ee30516697b9c7909e87a6562b20eb7
wuyufan <907871429@qq.com>
2024-07-29 22:58:07 +0800

    聊天室初始代码

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chatroom
 聊天室
+
+
+https://www.cnblogs.com/wupeixuan/p/13389757.html
---
+++ service/src/main/java/com/wuyufan/bean/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.wuyufan.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+    private String content;
+}
