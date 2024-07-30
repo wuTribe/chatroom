fc3487a2bc7fb0788719d0714bb06ebc5a5a5a07
wuyufan <907871429@qq.com>
2024-07-30 23:36:26 +0800

    聊天室修改

---
+++ service/src/main/java/com/wuyufan/bean/Result.java
@@ -0,0 +1,12 @@
+package com.wuyufan.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Result {
+    private boolean flag;
+    private String message;
+
+}
---
+++ service/src/main/java/com/wuyufan/bean/User.java
@@ -0,0 +1,9 @@
+package com.wuyufan.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+}
